d2c8b8cf412f8006bcff2cd9ddc7c3b4ec89d113
Weizhou Xue <benjamin_xue@outlook.com>
2026-05-26 14:02:10 +1200

    Fixed a blog-page routing bug

--- src/config/site.ts
+++ src/config/site.ts
@@ -4,9 +4,9 @@ export const site = {
   siteUrl: "https://weizhouxuebenjamin.github.io",
 
   username: "Benjamin Xue",
-  userTitle: "Software Engineer",
+  userTitle: "Full Stack Web Developer",
   userDescription:
-    "I'm a Software Engineer from Argentina, always searching for the next challenge; no task is too scary nor too difficult. Lately I have been immersed in world of Giving Circles working with Grapevine, a California based startup trying to improve the world through collaborative giving. I'm the first engineer they hired to work on the platform.",
+    "I'm a Full-stack engineer with 1+ year of production experience delivering React/Node.js applications for live charities and university teams. Built AI-powered OCR tools (95% precision), led 7‑dev teams through full SDLC, and shipped hybrid SSR/static sites improving SEO. Looking to scale impact in mission-driven or high‑autonomy engineering roles.",
 
   email: "benjamin_xue@outlook.com",
   social: {
